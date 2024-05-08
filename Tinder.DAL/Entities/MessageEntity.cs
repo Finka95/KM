29c6ed1dd10db0065a0bed545b51c742ef1928ca
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-05-08 12:57:34 +0300

    refactor

--- Tinder.DAL/Entities/MessageEntity.cs
+++ Tinder.DAL/Entities/MessageEntity.cs
@@ -3,7 +3,6 @@
 public class MessageEntity : BaseEntity
 {
     public string Text { get; set; }
-
     public DateTime Date { get; set; }
     public Guid ChatId { get; set; }
     public Guid UserSenderId { get; set; }
