bc0233417884e5671308918f914b910449c1c5f2
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-05-08 12:45:11 +0300

    create main entities

---
+++ Tinder.DAL/Entities/MessageEntity.cs
@@ -0,0 +1,14 @@
+﻿
+using Tinder.DAL.Interfaces;
+
+namespace Tinder.DAL.Entities;
+
+public class MessageEntity : IBaseEntity
+{
+    public Guid Id { get; set; }
+    public string Text { get; set; }
+
+
+    public Guid ChatId { get; set; }
+    //public Guid UserReceiverId { get; set; }
+}
