6d37648f9c225831bf8f8a1adad70cc32d1aec63
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-05-09 12:17:30 +0300

    updater DI

--- Tinder.BLL/DI/BusinessLayerRegister.cs
+++ Tinder.BLL/DI/BusinessLayerRegister.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tinder.DAL.DI;
 
 namespace Tinder.BLL.DI
 {
-    internal class BusinessLayerRegister
+    public static class BusinessLayerRegister
     {
+        public static void RegisterBusinessLogicDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.RegisterDataAccessDependencies(configuration);
+        }
     }
 }
--- Tinder.DAL/ApplicationDbContext.cs
+++ Tinder.DAL/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : 
         base(options)
     {
+        Database.Migrate();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
