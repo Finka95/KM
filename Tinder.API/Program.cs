6e34917bb2adaf464e1a452a3099088151fa0bd2
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-05-09 11:27:04 +0300

    set up database configuration

--- Tinder.API/Program.cs
+++ Tinder.API/Program.cs
@@ -1,9 +1,13 @@
+using Tinder.DAL.DI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.RegisterDataAccessDependencies(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ Tinder.DAL/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Tinder.DAL.Entities;
+
+namespace Tinder.DAL;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+            
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        /*modelBuilder.Entity<UserEntity>()
+            .HasMany(u => u.ReceivedLikes)
+            .WithMany(l => l.ReceiverUser);*/
+
+        modelBuilder.Entity<LikeEntity>()
+            .HasOne(l => l.SenderUser)
+            .WithMany(u => u.SentLikes)
+            .HasForeignKey(l => l.SenderId)
+            .OnDelete(DeleteBehavior.NoAction);
+            
+        modelBuilder.Entity<LikeEntity>()
+            .HasOne(l => l.ReceiverUser)
+            .WithMany(u => u.ReceivedLikes)
+            .HasForeignKey(l => l.ReceiverId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        base.OnModelCreating(modelBuilder); 
+    }
+
+    public DbSet<UserEntity> Users { get; set; }    
+    public DbSet<PhotoEntity> Photos { get; set; }
+    public DbSet<ChatEntity> Chats { get; set; }
+    public DbSet<LikeEntity> Likes { get; set; }
+    public DbSet<MessageEntity> Messages { get; set; }
+
+}
