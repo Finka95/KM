bb74db5644e627f58e178e08adf57cdfaaf8d552
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-05-15 10:58:33 +0300

    update IUserService

--- Tinder.BLL/Interfaces/IUserService.cs
+++ Tinder.BLL/Interfaces/IUserService.cs
@@ -1,6 +1,9 @@
-﻿namespace Tinder.BLL.Interfaces
+﻿using Tinder.BLL.Models;
+
+namespace Tinder.BLL.Interfaces
 {
-    public interface IUserService
+    public interface IUserService : IGenericService<User>
     {
+        
     }
 }
