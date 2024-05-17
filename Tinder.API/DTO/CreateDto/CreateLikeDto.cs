c7abaf100c09ce85ba98a074da842fd555a742b6
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-05-17 16:30:59 +0300

    refactor createLikeDto

--- Tinder.API/DTO/CreateDto/CreateLikeDto.cs
+++ Tinder.API/DTO/CreateDto/CreateLikeDto.cs
@@ -3,8 +3,6 @@
     public class CreateLikeDto
     {
         public Guid SenderId { get; set; }
-        //public UserDto SenderUser { get; set; }
         public Guid ReceiverId { get; set; }
-        //public UserDto ReceiverUser { get; set; }
     }
 }
