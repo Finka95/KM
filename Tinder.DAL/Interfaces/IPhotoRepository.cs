02c52cdc4c4f2878416523a1b70ed96d8e53ad77
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-05-20 23:26:31 +0300

    refactor

--- Tinder.DAL/Interfaces/IPhotoRepository.cs
+++ Tinder.DAL/Interfaces/IPhotoRepository.cs
@@ -5,7 +5,6 @@ namespace Tinder.DAL.Interfaces
     public interface IPhotoRepository : IGenericRepository<PhotoEntity>
     {
         Task<PhotoEntity> GetByIdAsync(Guid id, Guid userId, CancellationToken cancellationToken);
-        //Task<List<PhotoEntity>> UpdateRangeAsync(List<PhotoEntity> photos, CancellationToken cancellationToken);
         Task<PhotoEntity> DeleteAsync(PhotoEntity photo, CancellationToken cancellationToken);
     }
 }
