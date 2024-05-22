1ce9136148d252626e9be6b811b6bb9b058c9744
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-05-22 23:10:22 +0300

    add join chat function

--- Tinder.API/Hubs/ChatHub.cs
+++ Tinder.API/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using Tinder.API.Hubs.Connection;
 using Tinder.API.Hubs.Interfaces;
+using Tinder.BLL.Exceptions;
 using Tinder.BLL.Interfaces;
 
 namespace Tinder.API.Hubs
@@ -9,15 +10,22 @@ namespace Tinder.API.Hubs
     {
 
         private readonly IChatService _chatService;
+        private readonly IUserService _userService;
 
-        public ChatHub(IChatService chatService)
+        public ChatHub(IChatService chatService, IUserService userService)
         {
             _chatService = chatService;
+            _userService = userService;
         }
 
         public async Task JoinChat(ChatConnection chatConnection)
         {
-            var chat = await _chatService.GetByIdAsync(chatConnection.ChatId, cancellationToken: default);
+            var chat = await _chatService.GetByIdAsync(chatConnection.ChatId, cancellationToken: default) ?? throw new NotFoundException("Chat is not found");
+            var user = await _userService.GetByIdAsync(chatConnection.UserId, cancellationToken: default);
+
+            var roomName = chatConnection.ChatId.ToString();
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+            await Clients.Group(roomName).ReceiveMessage("MyApp", $"{user.FirstName} is in the chat waiting for your message");
         }
     }
 }
