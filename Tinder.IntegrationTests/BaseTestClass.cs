322a46ebc644fca43137add8e98d0f943a43741d
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-06-14 11:05:23 +0300

    make Program class public and add BaseTest class

---
+++ Tinder.IntegrationTests/BaseTestClass.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Tinder.IntegrationTests
+{
+    internal class BaseTestClass : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        protected readonly HttpClient _httpClient;
+
+        internal BaseTestClass(CustomWebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+    }
+}
