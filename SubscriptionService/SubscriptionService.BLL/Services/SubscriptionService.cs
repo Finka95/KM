5ee86ffadc259c53cca0b84420de2715bb9dfdd2
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-06-23 22:06:26 +0300

    Update SubscriptionService.cs

--- SubscriptionService/SubscriptionService.BLL/Services/SubscriptionService.cs
+++ SubscriptionService/SubscriptionService.BLL/Services/SubscriptionService.cs
@@ -7,23 +7,40 @@ using SubscriptionService.DAL.Interfaces;
 using System.Text.Json.Nodes;
 using SubscriptionService.Domain.Enums;
 using SubscriptionService.Domain.Exceptions;
+using Shared.Events;
+using SubscriptionService.BLL.MessageBroker.Interfaces;
 
 namespace SubscriptionService.BLL.Services
 {
     public class SubscriptionService : ISubscriptionService
     {
         private readonly ISubscriptionRepository _subscriptionRepository;
-        public SubscriptionService(ISubscriptionRepository subscriptionRepository)
+        private readonly IEventBus _eventBus;
+        public SubscriptionService(ISubscriptionRepository subscriptionRepository, IEventBus eventBus)
         {
             _subscriptionRepository = subscriptionRepository;
+            _eventBus = eventBus;
         }
 
-        public async Task<Subscription> CreateAsync(Subscription subscription, CancellationToken cancellationToken)
-        {
-            var modelToCreate = subscription.Adapt<SubscriptionEntity>();
-            modelToCreate.CreatedAt = DateTime.Now;
-            modelToCreate.ExpiresAt = DateTime.Now.AddMonths(1);
+        public async Task<Subscription> CreateAsync(Guid fusionUserId, Subscription subscription, CancellationToken cancellationToken)
+        { 
+            var userSubscription = _subscriptionRepository.GetByFusionUserIdAsync(fusionUserId, cancellationToken);
+            if (userSubscription != null)
+            {
+                throw new BadRequestException("User already has subscription");
+            }
+            
+            var modelToCreate = new SubscriptionEntity
+            {
+               FusionUserId = fusionUserId,
+               SubscriptionType = subscription.SubscriptionType,
+               CreatedAt = DateTime.Now,
+               UpdatedAt = DateTime.Now,
+               ExpiresAt = DateTime.Now.AddMonths(1)
+            };
             var entity = await _subscriptionRepository.CreateAsync(modelToCreate, cancellationToken);
+            var subscriptionCreated = entity.Adapt<SubscriptionCreated>();
+            await _eventBus.PublishAsync(subscriptionCreated, cancellationToken);
             return entity.Adapt<Subscription>();
         }
 
@@ -36,10 +53,10 @@ namespace SubscriptionService.BLL.Services
 
             var subscription = new Subscription
             {
-                Id = Guid.NewGuid(),
                 FusionUserId = fusionUserId,
-                Type = SubscriptionType.Base,
+                SubscriptionType = SubscriptionType.Base,
                 CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
                 ExpiresAt = DateTime.Now.AddMonths(1)
             };
 
@@ -56,6 +73,8 @@ namespace SubscriptionService.BLL.Services
                 return;
             }
             await _subscriptionRepository.DeleteAsync(subscriptionEntity, cancellationToken);
+            var subscriptionDeleted = subscriptionEntity.Adapt<SubscriptionDeleted>();
+            await _eventBus.PublishAsync(subscriptionDeleted, cancellationToken);
         }
 
         public async Task<List<Subscription>> GetAllAsync(CancellationToken cancellationToken)
@@ -66,8 +85,19 @@ namespace SubscriptionService.BLL.Services
 
         public async Task<Subscription> GetByIdAsync(Guid id, CancellationToken cancellationToken)
         {
-            var entity = await _subscriptionRepository.GetByIdAsync(id, cancellationToken) ?? throw new NotFoundException("Entity with this id doesn't exist"); ;
+            var entity = await _subscriptionRepository.GetByIdAsync(id, cancellationToken) ?? throw new NotFoundException("Entity with this id doesn't exist"); 
             return entity.Adapt<Subscription>();
         }
-    }
+        public async Task<Subscription> UpdateAsync(Guid id, SubscriptionType subscriptionType, CancellationToken cancellationToken)
+        {
+            var subscriptionToUpdate = await _subscriptionRepository.GetByIdAsync(id, cancellationToken) ?? throw new NotFoundException("Entity with this id doesn't exist");
+            subscriptionToUpdate.SubscriptionType = subscriptionType;
+            await _subscriptionRepository.UpdateAsync(id, subscriptionToUpdate, cancellationToken);
+
+            var updatedSubscription = await _subscriptionRepository.GetByIdAsync(id, cancellationToken);
+            var subscriptionUpdated = updatedSubscription.Adapt<SubscriptionUpdated>();
+            await _eventBus.PublishAsync(subscriptionUpdated, cancellationToken);
+            return updatedSubscription.Adapt<Subscription>();
+        }
+    }   
 }
