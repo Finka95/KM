53dc3fda10cf0b5bfb7c870f269d88b21c065df6
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-07-25 17:03:54 +0300

    update font family

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ApolloProvider } from '@apollo/client'
+import client from './components/apolloClient.ts'
 import { ThemeProvider, createTheme } from '@mui/material'
 
 const theme = createTheme({
   typography: {
-    fontFamily: 'kumbh sans',
+    fontFamily: 'Noi Grotesk',
   },
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>    
+    <ThemeProvider theme={theme}>
+      <ApolloProvider client={client}>
         <App />
+      </ApolloProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
